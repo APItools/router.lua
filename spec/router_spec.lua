8f5dff94b6462652e41a0d2d850921f5699572a8
kikito <kikito@gmail.com>
2015-03-31 01:07:21 +0200

    rewrite tests which were testing implementation

--- spec/router_spec.lua
+++ spec/router_spec.lua
@@ -16,52 +16,46 @@ describe("Router", function()
     describe('when first param is a string', function()
       it("understands fixed strings", function()
         r:match("get", "/foo", write_dummy)
-        assert.same(r._tree, {
-          get = { foo = { [LEAF] = write_dummy } }
-        })
+        r:execute("get", "/foo", "ok")
+        assert.same(dummy.params, "ok")
       end)
 
       it("understands chained fixed strings", function()
         r:match("get", "/foo/bar", write_dummy)
-        assert.same(r._tree, {
-          get = { foo = { bar = { [LEAF] = write_dummy } } }
-        })
+        r:execute("get", "/foo/bar", "ok")
+        assert.same(dummy.params, "ok")
       end)
 
       it("understands params", function()
         r:match("get", "/foo/:id", write_dummy)
-        local key, node = next(r._tree.get.foo)
-        assert.same(key, ":id")
-        assert.same(node, { [LEAF] = write_dummy })
+        r:execute("get", "/foo/bar")
+        assert.same(dummy.params, {id="bar"})
       end)
 
       it("does not duplicate the same node twice for the same param id", function()
         r:match("get", "/foo/:id/bar", write_dummy)
         r:match("get", "/foo/:id/baz", write_dummy)
-        local key, node = next(r._tree.get.foo)
-        assert.same(key, ":id")
-        assert.same(node, {
-          bar = {[LEAF] = write_dummy },
-          baz = {[LEAF] = write_dummy }
-        })
+
+        r:execute("get", "/foo/1/bar")
+        assert.same(dummy.params, {id="1"})
+
+        r:execute("get", "/foo/2/baz")
+        assert.same(dummy.params, {id="2"})
       end)
 
       it("supports an extension on a param", function()
         r:match("get", "/foo/:id.json", write_dummy)
-        local key, node = next(r._tree.get.foo)
-        assert.same(key, ":id")
-        assert.same(node, {
-          json = {[LEAF] = write_dummy }
-        })
+
+        r:execute("get", "/foo/1.json")
+        assert.same(dummy.params, {id="1"})
       end)
     end)
 
     describe('when first param is a table', function()
       it("understands fixed strings", function()
         r:match({ get = { ["/foo"] = write_dummy} })
-        assert.same(r._tree, {
-          get = { foo = { [LEAF] = write_dummy } }
-        })
+        r:execute("get", "/foo", "ok")
+        assert.same(dummy.params, "ok")
       end)
 
       it("understands chained fixed strings", function()
@@ -73,9 +67,8 @@ describe("Router", function()
 
       it("understands params", function()
         r:match({get = {["/foo/:id"] = write_dummy}})
-        local key, node = next(r._tree.get.foo)
-        assert.same(key, ":id")
-        assert.same(node, { [LEAF] = write_dummy })
+        r:execute("get", "/foo/bar")
+        assert.same(dummy.params, {id="bar"})
       end)
 
       it("does not duplicate the same node twice for the same param id", function()
@@ -85,12 +78,11 @@ describe("Router", function()
             ["/foo/:id/baz"] = write_dummy
           }
         })
-        local key, node = next(r._tree.get.foo)
-        assert.same(key, ":id")
-        assert.same(node, {
-          bar = {[LEAF] = write_dummy },
-          baz = {[LEAF] = write_dummy }
-        })
+        r:execute("get", "/foo/1/bar")
+        assert.same(dummy.params, {id="1"})
+
+        r:execute("get", "/foo/2/baz")
+        assert.same(dummy.params, {id="2"})
       end)
 
       it("supports an extension on a param", function()
@@ -99,11 +91,8 @@ describe("Router", function()
             ["/foo/:id.json"] = write_dummy
           }
         })
-        local key, node = next(r._tree.get.foo)
-        assert.same(key, ":id")
-        assert.same(node, {
-          json = {[LEAF] = write_dummy }
-        })
+        r:execute("get", "/foo/1.json")
+        assert.same(dummy.params, {id="1"})
       end)
     end)
   end)
